70917c757a3c0b0d4e934a9154cdb7a9130e409e
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-08-01 20:43:28 +0300

    Add project files.

---
+++ Methods.cs
@@ -0,0 +1,33 @@
+﻿void sayHello() // Şarkı sözü yazdıran method
+{
+    Console.WriteLine("So burn down this room");
+}
+
+int returnInteger() // Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalanı döndüren method
+{
+    Random rnd = new Random();
+    int randomNumber = rnd.Next(0, 100);
+    int bolum = randomNumber % 2;
+    return bolum;
+}
+
+int multiply(int a, int b) // Kullanıcıdan alınan 2 sayının çarpımını döndüren method
+{
+    return a * b;
+}
+
+void welcome(string name, string surname) // Kullanıcıdan isim ve soyisim bilgisi alıp bunu Hoş Geldiniz mesajında kullana method
+{
+    Console.WriteLine($"Hoş Geldiniz {name} {surname}");
+}
+
+sayHello();
+Console.WriteLine("Rastgele üretilen sayının 2'ye bölümünden kalan: " + returnInteger());
+Console.WriteLine("Lütfen çarpmak istediğiniz 2 sayı giriniz:");
+int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Yazdığınız sayıların çarpımı: " + multiply(first,second));
+Console.WriteLine("Lütfen isim ve soyisminizi yazınız");
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+welcome(firstName, lastName);
